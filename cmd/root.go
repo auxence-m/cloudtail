38bc5dc7127c7ea443417ab2736911648b629c39
Auxence-M <auxencemedja@gmail.com>
2026-01-29 22:07:41 -0500

    Added a custom color scheme to charmbracelet/fang

--- cmd/root.go
+++ cmd/root.go
@@ -4,17 +4,39 @@ import (
 	"context"
 	"os"
 
+	"image/color"
+
+	"charm.land/lipgloss/v2"
 	"github.com/charmbracelet/fang"
 	"github.com/spf13/cobra"
 )
+
+func DefaultColorScheme(c lipgloss.LightDarkFunc) fang.ColorScheme {
+	base := c(lipgloss.Black, lipgloss.White)
+	return fang.ColorScheme{
+		Base:           base,
+		Title:          lipgloss.BrightMagenta,
+		Description:    base, // flag and command descriptions
+		Program:        c(lipgloss.Blue, lipgloss.BrightBlue),
+		DimmedArgument: c(lipgloss.BrightWhite, lipgloss.BrightBlack),
+		Comment:        c(lipgloss.BrightWhite, lipgloss.BrightBlack),
+		Flag:           lipgloss.Green,
+		FlagDefault:    c(lipgloss.BrightWhite, lipgloss.BrightBlack), // flag default values in descriptions
+		Command:        c(lipgloss.Red, lipgloss.BrightRed),
+		QuotedString:   base,
+		Argument:       base,
+		ErrorHeader:    [2]color.Color{lipgloss.Black, lipgloss.Red},
+		ErrorDetails:   lipgloss.Red,
+	}
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "cloudtail",
 	Version: "1.0",
-	Short:   "cloudtail - display or tail logs from Google Cloud Logging",
+	Short:   "cloudtail displays or tail logs from Google Cloud Logging",
 	Long: `cloudtail is a lightweight cloud-native command-line tool written in Golang that allows users to display or tail logs from Google Cloud Logging (similar to Kubectl logs).
-It connects to the Google Cloud Logging API, fetches logs for a specific project based on filters (like severity, resource, or time range). 
+It connects to the Google Cloud Logging API, fetches logs for a specific project based on filters like severity, resource type, or time range. 
 It displays the logs or continuously streams them to the terminal or to an output file in near real-time.`,
 }
 
@@ -24,7 +46,8 @@ func Execute() {
 	// Add charmbracelet/fang
 	// charmbracelet/fang calls root.ExecuteContext()
 	err := fang.Execute(context.Background(), rootCmd,
-		fang.WithVersion(rootCmd.Version), fang.WithoutManpage(), fang.WithNotifySignal(os.Interrupt, os.Kill))
+		fang.WithVersion(rootCmd.Version), fang.WithoutManpage(), fang.WithNotifySignal(os.Interrupt, os.Kill), fang.WithColorSchemeFunc(DefaultColorScheme))
+
 	if err != nil {
 		os.Exit(1)
 	}
